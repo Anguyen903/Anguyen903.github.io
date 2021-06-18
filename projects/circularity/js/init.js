0c7c1ac835e8005ed703665120f6db659278de3c
Daniel Kogler <danielk373@gmail.com>
2021-06-18 10:00:16 -0500

    resetting circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,30 +20,13 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-        var circle;			// variable to hold a single circle when creating circles / iterating
-        var circles = [100];	// variable to store all circles in one Array
+
 
         // TODO 2 : Create a function that draws a circle 
-        // Code to draw a circle
-        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-        physikz.addRandomVelocity(circle, canvas);
-        view.addChild(circle);
-        circles.push(circle);
+        
 
-        // TODO 3 / 8 : Call the drawCircle() function
+        // TODO 3 / 8 : Call the drawCircle() function 
 
-        var loopsCompleted = 0; 
-while (loopsCompleted < 10) {
-    // do something
-    loopsCompleted++
-}
-
-for (var loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
-    // do something
-}
-
-
-        /* Your Function Calls HERE */
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -56,82 +39,35 @@ for (var loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            myArray[0] //--> returns the first value in myArray
-            myArray[1] //--> returns the second value in myArray
 
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
-	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
-}
-            drawCircle()
-
-            physikz.addRandomVelocity(circle, canvas, 10, 10);
-
+            
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-
-
+           
 
             // TODO 9 : Iterate over the array
-            for (var i = 0; i < myArray.length; i++) {
-                var eachValue = myArray[i];
-                
-                // code to repeat using eachValue
-            }
+           
             
-        
+        }
     
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-       
+        game.checkCirclePosition = function(circle) {
 
-            
-            // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            game.checkCirclePosition = function(circle) {
-// if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-if ( circle.x > canvas.width ) {
-    circle.x = 0;
-}
-
-
-
-            if (circle.x > canvas.width) {
+            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+            if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
             
-            game.checkCirclePosition = function(circle) {
-
-                // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-                if ( circle.x > canvas.width ) {
-                    circle.x = 0;
-                }
-                
-                // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-                
-                /* Your conditional statements HERE */
+            // TODO 7 : YOUR CODE STARTS HERE //////////////////////
             
-                // YOUR TODO 7 CODE ENDS HERE //////////////////////////
-            }
+
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
         }
-            // // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-	if ( circle.x > canvas.width ) {
-		circle.x = 0;
-	}
-
-    
-
-
+        
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
         /////////////////////////////////////////////////////////////
